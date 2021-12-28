e77bc2c2015228b45d82bae1fed7ce67660eb360
Bodikov Kuat <kuat.bodikov@gmail.com>
2021-12-28 23:48:42 +0500

    created random contacts logic

--- Contacts/Models/Person.swift
+++ Contacts/Models/Person.swift
@@ -11,5 +11,27 @@ struct Person {
     var name: String
     var surname: String
     var phone: String
-    var mail: String
+    var email: String
+    
+    var title: String {
+        "\(name) \(surname)"
+    }
+}
+
+
+extension Person {
+    static func getContact() -> [Person] {
+        var persons: [Person] = []
+        
+        for _ in 1...10 {
+            let person = Person(name: "\(DataManager.shared.names.randomElement() ?? "")",
+                                surname: "\(DataManager.shared.surnames.randomElement() ?? "")",
+                                phone: "\(DataManager.shared.phones.randomElement() ?? "")",
+                                email: "\(DataManager.shared.emails.randomElement() ?? "")")
+        
+            persons.append(person)
+        }
+        return persons
+    }
+    
 }
